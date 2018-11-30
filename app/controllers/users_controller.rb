a0fab8ec8d85bf839948c84a11badaa61b766268
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-11-30 13:29:46 +0100

    Implementation of zip response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,17 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.json
-  def show; end
+  def show
+    respond_to do |format|
+      format.html { render }
+      format.zip do
+        files = save_files_on_server @user.pictures
+        zip_data = create_temporary_zip_file files
+
+        send_data(zip_data, type: 'application/zip', filename: 'user.zip')
+      end
+    end
+  end
 
   # GET /users/new
   def new
@@ -61,6 +71,54 @@ class UsersController < ApplicationController
   end
 
   private
+
+  # Download active storage files on server in a temporary folder
+  #
+  # @param files [ActiveStorage::Attached::Many] files to save
+  # @return [Array<String>] files paths of saved files
+  def save_files_on_server(files)
+    # get a temporary folder and create it
+    temp_folder = File.join(Dir.tmpdir, 'user')
+    FileUtils.mkdir_p(temp_folder) unless Dir.exist?(temp_folder)
+
+    # download all ActiveStorage into
+    files.map do |picture|
+      filename = picture.filename.to_s
+      filepath = File.join temp_folder, filename
+      File.open(filepath, 'wb') { |f| f.write(picture.download) }
+      filepath
+    end
+  end
+
+  # Create a temporary zip file & return the content as bytes
+  #
+  # @param filepaths [Array<String>] files paths
+  # @return [String] as content of zip
+  def create_temporary_zip_file(filepaths)
+    require 'zip'
+    temp_file = Tempfile.new('user.zip')
+
+    begin
+      # Initialize the temp file as a zip file
+      Zip::OutputStream.open(temp_file) { |zos| }
+
+      # open the zip
+      Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+        filepaths.each do |filepath|
+          filename = File.basename filepath
+          # add file into the zip
+          zip.add filename, filepath
+        end
+      end
+
+      return File.read(temp_file.path)
+    ensure
+      # close all ressources & remove temporary files
+      temp_file.close
+      temp_file.unlink
+      filepaths.each { |filepath| FileUtils.rm(filepath) }
+    end
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
